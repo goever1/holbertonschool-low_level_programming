d3eeacbd369ab0087da70c552786a3bd92ffffbc
root <jorgefernandoochoa1@hotmail.com>
2023-02-21 15:43:13 -0800

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point. Determines if a random number is positive, negative, or zero
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l = 'a';
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+	return 0;
+}
