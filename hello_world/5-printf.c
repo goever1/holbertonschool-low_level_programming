95939f36b087f6e84316f0e9803e4a1b5d07ecf3
root <jorgefernandoochoa1@hotmail.com>
2023-02-21 13:23:43 -0800

    printf  c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(void)
 /**
- * main - Entry point
+ * main:Entry point
+ *
  * return: Always 0 (success)
  */
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
