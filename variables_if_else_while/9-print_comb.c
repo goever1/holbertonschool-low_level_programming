e101e05f508388ad15c5399414518a3bdf5c011a
root <jorgefernandoochoa1@hotmail.com>
2023-02-21 19:24:13 -0800

    numbers , + space

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,9 +10,8 @@ int main(void)
 
 	for (n = 0; n <= 9; n++)
 		putchar((n + '0'));
-	if (n == 9)
-		break;
-	putchar(',');
+	if (n < '9')
+		putchar(',');
 	putchar(' ');
 	putchar('\n');
 	return (0);
