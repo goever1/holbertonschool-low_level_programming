d7d4901fc616f3e5b0235942ddfcd2c9a2b5dce8
goever1 <30449167+goever1@users.noreply.github.com>
2023-02-22 17:09:16 -0500

    Update 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,22 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - Prints the alphabet
  *
- * Return: Always return 0.
+ * Return: void.
+ *
  */
-int main(void)
+
+void print_alphabet(void)
+
 {
-    print_alphabet();
-    return (0);
+
+char c;
+
+for  (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+
 }
