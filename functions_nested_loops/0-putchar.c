fccf53f0381b02ea86f087252d7c7e7e4531e178
root <jorgefernandoochoa1@hotmail.com>
2023-02-22 12:07:17 -0800

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,13 @@
 #include "main.h"
 /**
+ * _putchar - is to declrear the function before its use
+ *
  * main - Prints _putchar
  *
  * Returns: Always 0 (Success)
  */
+int _putchar(char c);
+
 int main(void)
 {
 	char *co = "_putchar";
