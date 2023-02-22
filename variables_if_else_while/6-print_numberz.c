f3f3fa912d07c7f69519ba1c05f8a28707f47474
root <jorgefernandoochoa1@hotmail.com>
2023-02-21 18:08:05 -0800

    one digit numbers

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#inlcude <stdio.h>
+/**
+ * main - Prints all the numbers form 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		putchar(n);
+	putchar("\n");
+	return (0);
+}
