8c141c78d3315956d1f444ab10a77dc6eb678035
root <jorgefernandoochoa1@hotmail.com>
2023-02-21 18:33:01 -0800

    wackwards alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet backwards
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l;
+
+	for (l == 'z'; l <= 'a'; l--)
+		putchar(l);
+	putchar('\n');
+	return (0)
+}
