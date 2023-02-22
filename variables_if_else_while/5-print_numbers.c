f90818f27bd3c20a259246e2908fcd59cef5ba5c
root <jorgefernandoochoa1@hotmail.com>
2023-02-21 16:47:09 -0800

    one digit numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints all one digit numbers
+ *
+ * retun: Always 0 (Success)
+ */
+int main(void)
+{
+	char n=1;
+	
+	for (n = 1; n <= 9; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
