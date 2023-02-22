28724316f6b4fff7ba4f9ea8734438e23896629c
root <jorgefernandoochoa1@hotmail.com>
2023-02-21 16:09:59 -0800

    alphabet not q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet exept for the q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	l = 'a';
+
+	while (l <= 'z')
+	{
+		if (l == 'q' || l == 'e')
+			l++;
+		else
+			putchar(l);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
