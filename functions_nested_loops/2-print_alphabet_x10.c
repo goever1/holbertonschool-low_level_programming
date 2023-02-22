e8015252a9a514d5d0e1bcb7f39513ab7d4c46ab
root <jorgefernandoochoa1@hotmail.com>
2023-02-22 15:27:07 -0800

    alphabet x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,12 @@
  */
 void print_alphabet_x10(void)
 {
-	char l = 'a';
-	
+	char l;	
 	int n;
 
 	for (n = 1 ; l <= 10; n++)
 	{
-		for (; l <= 'z'; l++)
+		for (l = 'a'; l <= 'z'; l++)
 			_putchar(l);
 		_putchar('\n');
 		l = 'a';
