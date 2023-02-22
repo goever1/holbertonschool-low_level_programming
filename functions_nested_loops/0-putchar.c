f3855570002737904a24d91e47dbe6e90c3f16f2
root <jorgefernandoochoa1@hotmail.com>
2023-02-22 11:48:41 -0800

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char *co = "_putchar";
+
+	while (*co)
+	{
+		_putchar(*co);
+	co++;
+	}
+	_putchar('\n')
 	return 0;
 }
