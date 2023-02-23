2b4745f400cc56436f2dee7b6b6f6bd88aaf8540
root <jorgefernandoochoa1@hotmail.com>
2023-02-23 08:26:33 -0800

    add integers

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,21 +1,12 @@
 #include "main.h"
 /**
- * add - Prints the addition of two integers
+ * add - It adds two integers
  *
  * Return: Prints the addition of two integers
  */
 int add(int n1, int n2)
 {
 	int r = n1 + n2;
-	 if (r >= 10 || r <= -10)
-	 {
-		 _putchar(r + '0');
-	 }
-	 else
-	 {
-		 _putchar((r / 10) + '0');
-		 _putchar((r % 10) + '0');
-	 }
-	 _putchar('\n');
-	 return (r);
+
+	return (r);
 }
