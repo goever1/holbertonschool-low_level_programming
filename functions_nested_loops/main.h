67dbf6a9e99fe7a1a1ed8f8b3c70bdff8486dab6
root <jorgefernandoochoa1@hotmail.com>
2023-02-23 06:36:40 -0800

    lol

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,7 +2,6 @@
 #define main_h
 /**
  * File: holberton.h
- * Auth: Brennan D Baraban
  * Desc: Header file containing declarations for all functions
  *       used in the 0x02-functions_nested_loops directory.
  */
