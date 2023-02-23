81fda20560ae41faa4888af2ea92d71eed5b9201
root <jorgefernandoochoa1@hotmail.com>
2023-02-22 17:38:02 -0800

    lowecasse police

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <=122)
+		return (0);
+	else
+		return (1);
+}
