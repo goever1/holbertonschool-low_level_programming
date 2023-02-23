f5f24291a65e97ec3b2265ec4cf054b3d90ec935
root <jorgefernandoochoa1@hotmail.com>
2023-02-22 19:45:51 -0800

    9x9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+void times_table(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 <= 9; n1++)
+	{
+		for (n2 = 0; n2 <= 9; n2++)
+		{
+			_putchar((n1 * n2) + '0');
+			if (n2 < 9)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
