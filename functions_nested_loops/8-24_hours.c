23915684d9a9a10687b74289864aca84f2e36bcd
root <jorgefernandoochoa1@hotmail.com>
2023-02-22 19:12:49 -0800

    las digit number

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * jack_bauer - prints the hour in 24h style
+ */
+void jack_bauer(void)
+{
+	int h1, m1, h2, m2;
+
+	for (h1 = 0; h1 <= 2; h1++)
+	{
+		for (h2 = 0; h2 <= 3; h2++)
+		{
+			for (m1 = 0; m1 <= 5; m1++)
+			{
+				for (m2 = 0; m2 <= 9)
+				{
+					_putchar(h1 + '0');
+				_putchar(h2 + '0');
+				_putchar(':');
+				_putchar(m1 + '0');
+				_putchar(m2 + '0');
+				}
+			}
+		}
+	}
+}
+
