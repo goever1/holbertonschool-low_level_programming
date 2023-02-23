541f59f8843c69e30c70a506fdcce497a1c31943
root <jorgefernandoochoa1@hotmail.com>
2023-02-22 18:34:11 -0800

    las digit number

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,16 @@
 #include "main.h"
 /**
- * print_last_digit - It prints the las digit of the number
- */ 
+ * print_last_digit - prints the last digit of a number
+ *
+ * @n: the number to extract the last digit from
+ *
+ * Return: the value of the last digit
+ */
 int print_last_digit(int n)
 {
-	_putchar((n % 10));
+	int ld;
+
+	ld = _abs(n % 10);
+	_putchar('0' + ld);
+	return (ld);
 }
