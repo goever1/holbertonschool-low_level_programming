709d49d0ea62c86d4c9f8296e7bbe961e1a18184
root <jorgefernandoochoa1@hotmail.com>
2023-02-22 18:18:46 -0800

    Absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - retunrs the absolute value of an integral
+ *
+ * Return: If n is smoller than 0 returns negative, otherwise positive
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
