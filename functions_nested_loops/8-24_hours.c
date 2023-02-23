c5f404e116ffbc2fe2275b0e320bf3a2e955202a
root <jorgefernandoochoa1@hotmail.com>
2023-02-22 19:26:38 -0800

    24 h

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -4,27 +4,19 @@
  */
 void jack_bauer(void)
 {
-	int h1, m1, h2, m2;
+	int h, m;
 
-	for (h1 = 0; h1 <= 2; h1++)
+	for (h = 0; h <= 23; h++)
 	{
-		for (h2 = 0; h2 <= 9; h2++)
+		for (m = 0; m <= 5; m++)
 		{
-			for (m1 = 0; m1 <= 5; m1++)
-			{
-				for (m2 = 0; m2 <= 9; m2++)
-				{
-					_putchar(h1 + '0');
-				_putchar(h2 + '0');
+			_putchar((h / 10) + '0');
+				_putchar((h % 10) + '0');
 				_putchar(':');
-				_putchar(m1 + '0');
-				_putchar(m2 + '0');
+				_putchar((m / 10)) + '0');
+				_putchar((m2 % 10)) + '0');
 				_putchar('\n');
-				}
-			}
 		}
 	}
-	if (h1 0== 2 && h2 == 3)
-		break;
 }
 
