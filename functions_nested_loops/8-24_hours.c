c69e5ba990b8069ba2f01e4989b233e901bb1f98
root <jorgefernandoochoa1@hotmail.com>
2023-02-22 19:28:33 -0800

    24 h

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,8 +13,8 @@ void jack_bauer(void)
 			_putchar((h / 10) + '0');
 				_putchar((h % 10) + '0');
 				_putchar(':');
-				_putchar((m / 10)) + '0');
-				_putchar((m2 % 10)) + '0');
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
 				_putchar('\n');
 		}
 	}
