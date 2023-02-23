3921fbde5912f463c42c96c212d6667f4eeba086
root <jorgefernandoochoa1@hotmail.com>
2023-02-23 07:27:23 -0800

    lol

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -7,12 +7,12 @@ int add(int n1, int n2)
 	int r = n1 + n2;
 	 if (r > 10 && r < -10)
 	 {
-		 -putchar(r + '0');
+		 _putchar(r + '0');
 	 }
 	 else
 	 {
-		 -putchar((r / 10) + '0');
-		 -putchar((r % 10) + '0');
+		 _putchar((r / 10) + '0');
+		 _putchar((r % 10) + '0');
 	 }
 	 -putchar('\n');
 }
