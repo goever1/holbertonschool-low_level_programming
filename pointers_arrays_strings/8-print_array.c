dfd3902812d257749b8f2571b7d5b77e690e7a24
root <jorgefernandoochoa1@hotmail.com>
2023-02-28 15:25:30 -0800

    print array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,8 +11,10 @@ void print_array(int *a, int n)
 	int i = 0;
 
 	for (;i < n; ++i)
+	{
 		printf("%d", a[i]);
-	if (i != n)
-		printf(", ");
+		if (i != n)
+			printf(", ");
+	}
 	printf("\n");
 }
