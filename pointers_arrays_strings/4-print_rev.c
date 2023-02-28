db52551a2c459fa72d4534d50f1970242864cd74
root <jorgefernandoochoa1@hotmail.com>
2023-02-28 12:27:41 -0800

    rev print

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <string.h>
 #include <stdio.h>
+/**
+ * print_rev - It prints a string in reverse
+ * @s: it points the string
+ */
 void print_rev(char *s)
 {
 	size_t l = strlen(s);
