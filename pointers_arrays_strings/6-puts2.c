6611fca12338974ea4ada9658efa27751b467ce7
root <jorgefernandoochoa1@hotmail.com>
2023-02-28 14:20:18 -0800

    print by jumping 2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <string.h>
 #include <stdio.h>
+/**
+ * puts2 - Prints every character of a string in jumping in by 2
+ * @str: points the string
+ */
 void puts2(char *str)
 {
 	size_t l = strlen(str);
