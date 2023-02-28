f18bc8c10bb119624647a4b483a80e82c534daee
root <jorgefernandoochoa1@hotmail.com>
2023-02-28 15:47:45 -0800

    copy and paste an string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -5,8 +5,14 @@
  * *_strcpy - It copies the string of "src" and paste it to "dest"
  * @dest: the destination of the copy
  * @src: the original string
+ *
+ * Return: the dest array
  */
 char *_strcpy(char *dest, char *src)
 {
-	return strcpy(dest, src);
+	int i = 0;
+
+	for (; src[i] != '\0'; ++i)
+		dest[i] = src[i];
+	return (dest)
 }
