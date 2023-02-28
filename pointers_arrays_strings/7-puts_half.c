fb12e2679b35f8d99bb4c55ff21aebdcd5882725
root <jorgefernandoochoa1@hotmail.com>
2023-02-28 14:37:31 -0800

    print 2 half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * puts_half - It prints the 2 half of the string if odd i = (l - 1) / 2 
+ * @str: points the string
+ */
+void puts_half(char *str)
+{
+	size_t s = strlen(str);
+
+	int i = 0, l = s;
+
+	if (l % 2 = 0)
+	{
+		i = l / 2
+		for (; i < l; ++i)
+			printf("%c", str[i]);
+	else
+	{
+		i = (l - 1) / 2
+		for (; i < l; ++i)
+			printf("%c", str[i]);
+}
+
