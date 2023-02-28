b7dae3b9585b70a79c95a544d3cdc4e11a53e4b5
root <jorgefernandoochoa1@hotmail.com>
2023-02-28 11:31:28 -0800

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen - It returns the length of the string in *s
+ * @s: is the location of the string
+ * @l: is the variable to hold the output of strlen
+ */
+int _strlen(char *s)
+{
+	int l = strlen(*s);
+	return (l);
+}
