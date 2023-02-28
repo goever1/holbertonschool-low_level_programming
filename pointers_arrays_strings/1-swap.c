726d90d6c0242e98a17ecf16bad883821c985766
root <jorgefernandoochoa1@hotmail.com>
2023-02-28 11:10:47 -0800

    swaping

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * swap_in - It isswaping the values of a and b
- * @a: firt integrer
- * @b: second integer
- * @c: it is used to save the value of a to give it to b
+ * @*a: firt integrer
+ * @*b: second integer
+ * @*c: it is used to save the value of a to give it to b
  */
 void swap_int(int *a, int *b)
 {
-	int c;
-	c = a;
-	a = b;
-	b = c;
+	int *c;
+	*c = *a;
+	*a = *b;
+	*b = *c;
 }
 
