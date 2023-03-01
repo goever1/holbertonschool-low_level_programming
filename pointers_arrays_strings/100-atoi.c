3779fcf99389c5794e60da140980794e32b9b371
root <jorgefernandoochoa1@hotmail.com>
2023-02-28 16:51:18 -0800

    atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _atoi - Change the string to the number that the string contains
+ * @s: It is poiting the string
+ */
+int _atoi(char *s)
+{
+	int n = atoi(s);
+	return (0);
+}
