c5cb817c6a58ec9e52e08065e6dc128660706c0a
root <jorgefernandoochoa1@hotmail.com>
2023-02-28 17:33:26 -0800

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,20 @@
  */
 int _atoi(char *s)
 {
-	int val = atoi(s);
+	size_t l1 = strlen(s);
 
-	return(printf("%d", val));
+	int l = l1; i = 0, sign = 1, r = 0
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	for (; s[i] != '\0'; ++i)
+	{
+		if (s[i] < 0 || s[i] > 9)
+			return (0);
+		r = r * 10 + (si[i] - '0');
+	}
+	return(sign * r);
 }
