241a78bd3091d83831b8890944580dc5d49160ac
root <jorgefernandoochoa1@hotmail.com>
2023-03-01 07:24:42 -0800

    concatenate

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,24 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
  * *_strcat - It concatenates two strings
  * @dest: It is the destination of the procces
  * @src: points the string that is going to be concatenated
+ *
+ * Return: It returns the concatenation of the two pointers
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	size_t l1 = strlen(dest), l2 = strlen(src)
+
+	int ld = l1, i = 0, ls = l2, r = (ls + ld)
+	
+	ld = (' ');
+	for(; ld + 1 < r; ++ld)
+	{
+		dest[ld] = src[i];
+		++i;
+	}
 	return (dest);
 }
