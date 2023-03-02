f449cf2b5213ada4fe7f9e3a261e8023dd83af0e
root <jorgefernandoochoa1@hotmail.com>
2023-03-02 14:58:24 -0800

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * _strcmp - compare the to strings in a ascii base
+ * @s1:first string
+ * @s2: second string
+ *
+ * Return: The diference of the 2 strings
+ */
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2 && (*s1 == *s2))
