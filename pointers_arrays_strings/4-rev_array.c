d3e4df35013d8e053d8875fda0d99ea5371fa136
root <jorgefernandoochoa1@hotmail.com>
2023-03-02 15:14:52 -0800

    revers array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * reverse_array - revers an array
+ * @a is the input array
+ * @n the number of element in the array
+ */
 void reverse_array(int *a, int n)
 {
 	int i = 0, front = 0;
@@ -6,7 +11,7 @@ void reverse_array(int *a, int n)
 	for (; i < n; ++i)
 	{
 		front = a[i];
-		a[i] = a[n - i];
-		a[n - i] = front;
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = front;
 	}
 }
