be55a7a5557c172146fd56a8426e7430ca59db75
root <jorgefernandoochoa1@hotmail.com>
2023-03-02 14:15:04 -0800

    dont know

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0, deca = (*(&dest + 1) - dest);
 	
-	for (; i <= deca; ++i)
+	for (; i < deca; ++i)
 	{
 		if (n >= i)
 			dest[i] = src[i];
 	}
-	
+	dest[deca] = '\0';
 	return (dest);
 }
