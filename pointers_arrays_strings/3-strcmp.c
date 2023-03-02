1cf4de885df552db9b9dd9552a72330525d14059
root <jorgefernandoochoa1@hotmail.com>
2023-03-02 14:54:38 -0800

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
-int_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2 && (*s1 == *s2))
 	{
 		s1++;
 		s2++;
 	}
-	return *s1 - *s2;
+	return (*s1 - *s2);
 }
