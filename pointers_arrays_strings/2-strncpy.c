1c29094f0ea51429c083b9f459d61ed2346c8991
root <jorgefernandoochoa1@hotmail.com>
2023-03-02 14:12:07 -0800

    dont know

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,18 +4,19 @@
  * *_strncpy - concatenates the text adding null character
  * @dest: destination
  * @src: source
+ * @n: the amount of characters to be copy
  *
- * Retunr: The string cancatenated
+ * Return: The string cancatenated
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0, deca = (*(&dest + 1) - dest);
 	
 	for (; i <= deca; ++i)
-		{
-			if (n >= i)
-				dest[i] = *(src + i);
-		}
+	{
+		if (n >= i)
+			dest[i] = src[i];
+	}
 	
 	return (dest);
 }
