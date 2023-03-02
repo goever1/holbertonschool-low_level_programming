e304a5b7cb1eca3e713ab1a5c3c85a4c80fff37a
root <jorgefernandoochoa1@hotmail.com>
2023-03-02 13:25:18 -0800

    dont know

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	size_t l1 = strlen(dest);
 
-	int l = l1, i = 0, deca = (*(&dest + 1) - dest); srca = (*(&src) + 1)
+	int l = l1, i = 0, deca = (*(&dest + 1) - dest); srca = (*(&src) + 1);
 	
 	for (; l < n; ++l)
 		{
