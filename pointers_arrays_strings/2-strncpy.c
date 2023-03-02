9e4886f30ac10ec744a6824b3de3e466306cdaba
root <jorgefernandoochoa1@hotmail.com>
2023-03-02 13:12:30 -0800

    dont know

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -5,14 +5,13 @@ char *_strncpy(char *dest, char *src, int n)
 	size_t l1 = strlen(dest);
 
 	int l = l1, i = 0, mrk = (l + n), arca = (*(&dest + 1) - dest);
-
-	for (; l < mrk; ++l)
-	{
-		if (arca < mrk)
-			break;
-		dest[l] = src[i];
-		++i;
-	}
+	
+	if (arca > mrk)
+		for (; l < mrk; ++l)
+		{
+			*(dest + l) = *(src + i);
+			++i;
+		}
 	dest[arca] = '\0';
 	return (dest);
 }
