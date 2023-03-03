55b58141c9c6273c7b3fcc394d09a6affcf835e1
root <jorgefernandoochoa1@hotmail.com>
2023-03-02 17:58:07 -0800

    coding 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+char *leet(char *str)
+{
+	char cod = "4BCD3F6H1JKLMN0PQR57UVWXY2";
+	int i = 0, j = 0;
+
+	for (; str[i] != '\0'; ++i)
+	{
+		for (; j < 26; ++j)
+		{
+			if (str[i] == 'a' + j || str[i] == 'A' + j)
+				str[i] = cod[j];
+		}
+	}
+	str[i] = '\0';
+	return (str);
+}			
