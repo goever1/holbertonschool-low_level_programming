52af221325bf3c99b540e22b95449b223628f6e3
root <jorgefernandoochoa1@hotmail.com>
2023-03-03 08:40:14 -0800

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,19 @@
 #include "main.h"
 #include <string.h>
+/**
+ * *_strchr - retunr a pinter if the character in c is found in the string
+ * @s: the string to find the character
+ * @c: the charachter to be found
+ *
+ * Return: null if the character is found or the pointer if its found
+ */
 char *_strchr(char *s, char c)
 {
 	int i = 0, l = strlen(s);
 
 	for (; i < l; ++i)
 		if (s[i] == c)
-			return &s[i];
+			return (&s[i]);
 	if (c == '\0' && s[i] == '\0')
 		return (&s[i]);
 	return (NULL);
