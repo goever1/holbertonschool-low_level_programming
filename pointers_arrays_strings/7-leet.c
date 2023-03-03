4e7deede1bf749498136c55845355ddaa17e155f
root <jorgefernandoochoa1@hotmail.com>
2023-03-02 18:35:56 -0800

    coding 1337

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,17 +1,26 @@
 #include "main.h"
+/**
+ * *leet - is chganging some letters of the input to numbers
+ * @str: the input
+ *
+ * Return: the string modified
+ */
 char *leet(char *str)
 {
-	char *cod = "4BCD3F6H1JKLMN0PQR57UVWXY2";
-	int i = 0, j = 0;
+	int i = 0, j = 0, code[] = {'4', '3', '0', '7', '1'}
+	char let[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'}
 
 	for (; str[i] != '\0'; ++i)
 	{
-		for (; j < 26; ++j)
+		for (j = 0; j < 10; ++j)
 		{
-			if (str[i] == 'a' + j || str[i] == 'A' + j)
-				str[i] = cod[j];
+			if (str[i]) == let[j])
+			{
+				str[i] = code[j / 2];
+				j = 10;
+			}
 		}
 	}
 	str[i] = '\0';
 	return (str);
-}			
+}
