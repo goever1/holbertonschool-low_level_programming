99c70a9b377e430f1ddf851334d9251799f67eac
root <jorgefernandoochoa1@hotmail.com>
2023-03-03 15:32:13 -0800

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -3,15 +3,16 @@
  * _strspn - returns the amount of times a character of a string is found
  * @accept: the character to be count
  * @s: the string to capare
+ *
  * Return: the number of caracter that are found
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0, j = 0, cont = 0;
+	int i = 0, cont = 0;
 
 	for (; s[i] != '\0'; ++i)
 	{
-		for (; accept[j] != '\0'; ++j)
+		for (j = 0; accept[j] != '\0'; ++j)
 		{
 			if (s[i] == accept[j])
 				cont++;
