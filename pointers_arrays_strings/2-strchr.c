2cda45b7336e97ba322703f2506c5cc444acac84
root <jorgefernandoochoa1@hotmail.com>
2023-03-03 08:26:13 -0800

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,11 +2,10 @@
 #include <string.h>
 char *_strchr(char *s, char c)
 {
-	size_t l1 = strlen(s);
-	int i = 0, l = l1;
+	int i = 0, l = strlen(s);
 
 	for (; i < l; ++i)
 		if (s[i] == c)
 			return &s[i];
-	return ('\0');
+	return (&s[i]);
 }
