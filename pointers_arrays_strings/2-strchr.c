1a20ecf13374bb1090cb2622cc2d872f75abe8df
root <jorgefernandoochoa1@hotmail.com>
2023-03-03 07:57:48 -0800

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+char *_strchr(char *s, char c)
+{
+	size_t l1 = strlen(s)
+	int i = 0, j = 0 l = l1, *p;
+
+	for (; i < l; ++i)
+		if (s[i] == c)
+		{
+			*p = s[i];
+			j++
+		}
+	if (j == 0)
+		*p = '\0';
+	return (*p);
+}
