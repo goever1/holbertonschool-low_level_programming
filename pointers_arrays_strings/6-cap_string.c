08a38a83c4ec753a1e3d186c06ec568c49442977
root <jorgefernandoochoa1@hotmail.com>
2023-03-02 16:34:51 -0800

    cap all words

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,18 +8,19 @@
 char *cap_string(char *n)
 {
 	int i = 0, cap = 0, x = 0;
-	int separators[] = {',', ';', '.', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'};
+	int separators[] = {',', ';', '.', '?', '"', '('};
+	int separators1[] = {')', '{', '}', ' ', '\n', '\t'};
 
 	for (; n[i] != '\0'; ++i)
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
 			n[i] = n[i] - cap;
 		cap = 0;
-		for (x = 0; x <= 12; x++)
+		for (x = 0; x <= 6; x++)
 		{
 			if (n[i] == separators[x])
 			{
-				x = 12;
+				x = 6;
 				cap = 32;
 			}
 		}
