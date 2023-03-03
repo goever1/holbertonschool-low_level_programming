39bde0bad7e819eeff42c15f657e74ba3379c0e8
root <jorgefernandoochoa1@hotmail.com>
2023-03-03 07:39:47 -0800

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,9 +1,17 @@
 #include "main.h"
+/**
+ * *_memset - Give the valie of b to the memory space that is pointed in s
+ * @s: the pointer to be modified
+ * @b: the value to use as a modifier
+ * @n: the amount of memory holders to be modified
+ *
+ * Return: It returns 's' after the modifications
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0, cha = n;
 
-	for (;i < cha; ++i)
+	for (; i < cha; ++i)
 	{
 		s[i] = b;
 	}
