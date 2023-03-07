a60ef012b50aa465666d1fe16ad942cf5ae29595
root <jorgefernandoochoa1@hotmail.com>
2023-03-07 08:31:53 -0800

    print

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,5 +1,10 @@
 #include "main.h"
+/**
+ * _puts_recursion - it prints a string follow by a new line
+ * @s: It is pointing to the original string
+ */
 void _puts_recursion(char *s)
 {
-	return (s, '\n');
+	_putchar(s);
+	_putchar('\n');
 }
