3174c4e22315042783deddc47fa66a2e7537d14f
root <jorgefernandoochoa1@hotmail.com>
2023-03-09 11:20:57 -0800

    name of the program

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,10 +3,12 @@
  * main  - prints it name follow by a \n
  * @argc: number of arguments
  * @argv: it is pointing to arguments
+ * Return: IT return 0
  */
-char main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
 	argc = 0;
 
 	printf("%s\n", argv[argc]);
+	return(0)
 }
