4ec273ff7506cf8b54ea6d2d6b9721c66977147a
root <jorgefernandoochoa1@hotmail.com>
2023-03-09 13:16:29 -0800

    multiplie

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds two positive numbers
+ * @argc: amount of arguments
+ * @argv: pointing to the arguments
+ * Return: 0 when there are no numbers to add or success, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0, r = 0;
+	
+	if (argc < 1')
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; ++i)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		r += atoi(argv[i]);
+	}
+	printf("%d\n", r);
+	return (0);
+}
