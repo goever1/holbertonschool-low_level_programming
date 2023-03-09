d62847ecc935ecd2fd0e222f9f2874c26bdb8750
root <jorgefernandoochoa1@hotmail.com>
2023-03-09 12:51:52 -0800

    multiplie

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - multiplies two numbers
  * @argc: variable not used
@@ -7,13 +8,16 @@
  */
 int main(__attribute__((unused)) int argc, char *argv[])
 {
-	int n1 = *argv[1], n2 = *argv[2], r = 0;
-	r = n1 * n2;
-	if (n1 == '\0' && n2 == '\0')
+	int n1 = 0, n2 = 0, r = 0;
+	
+	if (argc < 3)
 	{
-		printf("%s\n", "Error");
+		printf("Error: two numbers are required\n");
 		return (1);
 	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	r = n1 * n2;
 	printf("%d\n", r);
 	return (0);
 }
