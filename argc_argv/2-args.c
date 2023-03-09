4739dfefbff0a2ed337e6fa8dcb53428d12924d4
root <jorgefernandoochoa1@hotmail.com>
2023-03-09 12:28:43 -0800

    print arguments

---
+++ argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the arguments in the program
+ * @argc: number of arguments
+ * @argv: pointer to the arguments
+ * Return: 0 when success
+ */
+int main (int argc,char *argv[])
+{
+	int i = 0;
+
+	for (; i < argc; ++i)
+		printf("%s\n", argv[i]);
+	return (0);
+}
