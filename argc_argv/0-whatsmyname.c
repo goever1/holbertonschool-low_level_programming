25b19f0c09241014a3b781f5ad6f264aba2f8279
root <jorgefernandoochoa1@hotmail.com>
2023-03-09 11:17:45 -0800

    argc_argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main  - prints it name follow by a \n
+ * @argc: number of arguments
+ * @argv: it is pointing to arguments
+ */
+char main (int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+}
