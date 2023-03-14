a039416e3e8a90087339cfc799ee0042e041ab71
root <jorgefernandoochoa1@hotmail.com>
2023-03-14 14:27:59 -0700

    concatenates

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,8 +12,10 @@ char *str_concat(char *s1, char *s2)
 	int leng1 = 0, leng2 = 0, i = 0;
 	char *po;
 	
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (; s1[leng1] != '\0'; ++leng1)
 	{
 	}
