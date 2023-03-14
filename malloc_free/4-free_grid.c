767af22da1c9597f9f8ea350dcb5e9d96aeb1e88
root <jorgefernandoochoa1@hotmail.com>
2023-03-14 15:45:16 -0700

    freedom

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -11,9 +11,9 @@ void free_grid(int **grid, int height)
 {
 	int i = 0;
 
-	if (grid != null || heigth != 0)
+	if (grid != NULL || height != 0)
 	{
-		for(; i <= heigth; ++i)
+		for(; i <= height; ++i)
 			free(grid[i]);
 		free(grid);
 	}
