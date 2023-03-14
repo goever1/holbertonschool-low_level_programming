862e1885406d0dea6d9b43cc1b950ea6602c6878
root <jorgefernandoochoa1@hotmail.com>
2023-03-14 14:57:32 -0700

    2D array

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlin.h>
+/**
+ * alloc_grid - it inizialice all grid positions with 0
+ * @width: width input
+ * @heigth: height input
+ * Return: null when fail, the grid with 0 when success
+ */
+int **alloc_grid(int width, int height)
+{
+	int i = 0, j = 0, **po;
+	if (width <= 0 || heigth <= 0)
+		return (NULL);
+	po = malloc(sizeof(int) * height);
+	if (po == NULL)
+		return (NULL);
+	for (; i < height; ++i)
+	{
+		po[i] = malloc(sizeof(int) * width);
+		for (; j < width; ++j)
+			po[i][j] = 0;
+	}
+	return (po);
+}
