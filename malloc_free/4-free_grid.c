593955b4e9df8f7958b7cffdd4912daa87e5ddaf
root <jorgefernandoochoa1@hotmail.com>
2023-03-14 15:46:46 -0700

    freedom

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
 /**
  * free_grid - it frees the memory location of the grid
  * @grid: input grid
- * @heigth: inpute size of the grid
+ * @height: inpute size of the grid
  * Return: A free grid
  */
 void free_grid(int **grid, int height)
@@ -13,7 +13,7 @@ void free_grid(int **grid, int height)
 
 	if (grid != NULL || height != 0)
 	{
-		for(; i <= height; ++i)
+		for (; i <= height; ++i)
 			free(grid[i]);
 		free(grid);
 	}
