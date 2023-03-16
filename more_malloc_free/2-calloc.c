6d1ba2353324981f150aa4f44c139bdc79dcccc9
root <jorgefernandoochoa1@hotmail.com>
2023-03-16 16:20:22 -0700

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i = 0;
 	char *po;
 	
-	if (nmemb == '0' || size == '0')
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	po = malloc(nmemb * size);
 	if (po == NULL)
