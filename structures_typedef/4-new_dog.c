0fb1995d267596039ce798b967125ae3da3418bb
root <jorgefernandoochoa1@hotmail.com>
2023-03-21 13:29:05 -0700

    copy

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,16 +15,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int owner_len = 0, name_len = 0, i = 0;
 	if ( name == NULL || age <= 0 || owner == NULL)
 		return (NULL);
-	for (; name[name_len] != "\0"; ++name_len)
+	for (; name[name_len] != '\0'; ++name_len)
 		;
-	for (; owner[owner_len] != "\0"; ++owner_len)
+	for (; owner[owner_len] != '\0'; ++owner_len)
 		;
 	for (i = 0; i < name_len; ++i)
 		cp_name[i] = name[i];
 	for (i = 0; i < owner_len; ++i)
-		cp_ownen[i] = owner[i];
+		cp_owner[i] = owner[i];
 	new_name->name = cp_name;
-	new_name->owner = cp_pwner;
+	new_name->owner = cp_owner;
 	new_name->age = age;
 	return (new_name);
 }
