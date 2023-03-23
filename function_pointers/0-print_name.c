7fd33be5d9c1f99342577f44f398486f8c02325b
root <jorgefernandoochoa1@hotmail.com>
2023-03-23 10:03:33 -0700

    print name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name - it prints a name
  * @name: name
- * @f_ pointer to a function
+ * @f: pointer to a function
  * Return: null if fail
  */
 void print_name(char *name, void (*f)(char *))
