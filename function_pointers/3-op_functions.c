19580107d7cf7e74921438680b838aa0dd1c1687
root <jorgefernandoochoa1@hotmail.com>
2023-03-23 14:46:45 -0700

    fix error name

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - adds two integers
+ * @a: #1
+ * @b: #2
+ * Return: the two numbers added
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts two numbers
+ * @a: #1
+ * @b: #2
+ * Return: the substraction
+ */
+int op_sub(int a, int b)
+{
+	return ( a - b);
+}
+/**
+ * op_mul - multiplies two numbers
+ * @a: #1
+ * @b: #2
+ * Return: the product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two numbers
+ * @a: #1
+ * @b: #2
+ * Return: the division
+ */
+int op_div(int a, int b)
+{
+	return (a / b)
+}
+/**
+ * op_mod - it divides two numbers
+ * @a: #1
+ * @b: #2
+ * Return: the remainder of the division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b)
+}
