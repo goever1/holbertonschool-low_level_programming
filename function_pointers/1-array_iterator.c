c62b60882035fbf0b9f564d9932bf70b252d532b
root <jorgefernandoochoa1@hotmail.com>
2023-03-23 10:13:18 -0700

    array parameters

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - execute a function using all array as parameter
+ * @array: function arguments
+ * @size: size of array
+ * @action: Function to execute
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (array != NULL || action != NULL)
+	{
+		for (; i <= size; ++i)
+			action(array[i]);
+	}
+}
