dbd097f170893e3246d3afb5440dc85f8f33d5bf
root <jorgefernandoochoa1@hotmail.com>
2023-03-23 10:14:41 -0700

    array parameters

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array != NULL || action != NULL)
 	{
-		for (; i <= size; ++i)
+		for (; i < size; ++i)
 			action(array[i]);
 	}
 }
