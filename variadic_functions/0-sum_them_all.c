3ba7d6b463b6fb10974143d0140d9e108145363e
root <jorgefernandoochoa1@hotmail.com>
2023-03-24 12:34:04 -0700

    betty fix

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -4,18 +4,18 @@
 /**
  * sum_them_all - sums all numbers
  * @n: variadic inputs
- * Return 0 when n = 0, the sum of all numbers if success
+ * Return: 0 when n = 0, the sum of all numbers if success
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i = 0, res = 0;
 	va_list nums;
-	
+
 	if (n == 0)
 		return (0);
 	va_start(nums, n);
 	for (; i < n; ++i)
-		res = va_arg(nums, unsigned int);
+		res += va_arg(nums, unsigned int);
 	va_end(nums);
 	return (res);
 }
