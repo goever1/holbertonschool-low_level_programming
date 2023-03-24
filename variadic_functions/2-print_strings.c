5bb3e0d2f0bbed580323e9e84cf8cb60910b2396
root <jorgefernandoochoa1@hotmail.com>
2023-03-24 13:27:48 -0700

    1.2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (s != NULL)
 			printf("%s", s);
 		else
-			printf("(nil)%s");
+			printf("(nil)");
 		if (i != (n - 1))
 			printf("%s", separator);
 	}
