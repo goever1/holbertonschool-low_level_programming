ffbf1093f2d35871054676deef95d1829c6cd777
root <jorgefernandoochoa1@hotmail.com>
2023-04-06 15:09:23 -0700

    list length

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"                                                                                                                                                                              
+/**                                                                                                                                                                                             
+ * slistint_len - count the number of elements in a list                                                                                                                                        
+ * @h: the list                                                                                                                                                                                 
+ * Return: the number of elements in a list                                                                                                                                                     
+ */                                                                                                                                                                                             
+size_t dlistint_len(const dlistint_t *h)                                                                                                                                                        
+{
+	int i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+       return (i);
+}
