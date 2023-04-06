dd46a94611b57c771ac4fc031000682245150497
root <jorgefernandoochoa1@hotmail.com>
2023-04-06 12:13:06 -0700

    fiz betty

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * add_node_end - adds a node at the end of a list
+ * @head: linked list
+ * @str: string to add
+ * Return: address of the head.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *temp;
+	int i = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	for (; str[i]; i++)
+		;
+	new->len = i;
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while(temp->next != NULL)
+			temp = temp ->next;
+		temp->next = new;
+	}
+	return (*head);
+}
