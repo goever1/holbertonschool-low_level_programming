a77b32a89daf249b0440e6f700260ba7e9acf505
root <jorgefernandoochoa1@hotmail.com>
2023-04-06 12:20:48 -0700

    fix betty

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -26,8 +26,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	else
 	{
-		while(temp->next != NULL)
-			temp = temp ->next;
+		while (temp->next != NULL)
+			temp = temp->next;
 		temp->next = new;
 	}
 	return (*head);
