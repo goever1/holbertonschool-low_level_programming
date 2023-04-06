8dd85f839f7581b7c787406240d7681c41de00b3
root <jorgefernandoochoa1@hotmail.com>
2023-04-06 10:40:17 -0700

    count

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * list_len - counts the number of elements in a list
+ * @h: linked list
+ * Return: The number of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	for (; h != NULL; ++i)
+		h = h->next;
+	return (i);
+}
