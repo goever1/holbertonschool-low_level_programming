b68a936f0c333d5f8dd5bdc5b08264ba7c0825bb
root <jorgefernandoochoa1@hotmail.com>
2023-04-06 10:30:23 -0700

    single linked lists

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_list - print all the elements of a list_t
+ * @h: single linked list.
+ * Return: number of elements in the list
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	for (; h != NULL; ++i)
+	{
+		if (str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (i);
+}
