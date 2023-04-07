30614e7ed6f06d016eeb3786b513d681b496ac08
root <jorgefernandoochoa1@hotmail.com>
2023-04-07 13:34:20 -0700

    sum list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_dlistint - it adds all the values in the list
+ * @head: the list
+ * Return: the result when success, 0 when fail
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int res = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head)
+	{
+		res += head->n;
+		head = head->next
+	}
+	return (res);
+}
