5876dd1355e820e3202c4040571ed44f9cd0b394
root <jorgefernandoochoa1@hotmail.com>
2023-04-07 16:28:57 -0700

    delete node

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -8,22 +8,23 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 1;
-
+	
 	if (index == 0)
 	{
 		((*head)->next)->prev = NULL;
 		free(*head);
+		return (1);
 	}
 	for (; (*head) != NULL; ++i)
+	{
+		if (i == index)
 		{
-			if (i == index)
-			{
-				((*head)->next)->prev = (*head)->prev;
-				((*head)->prev)->next = (*head)->next;
-				free(*head);
-				return (1);
-			}
-			(*head) = (*head)->next;
+			((*head)->next)->prev = (*head)->prev;
+			((*head)->prev)->next = (*head)->next;
+			free(*head);
+			return (1);
 		}
+		(*head) = (*head)->next;
+	}
 	return (-1);
 }
