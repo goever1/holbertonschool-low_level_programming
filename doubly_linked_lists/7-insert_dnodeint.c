42bbaf8d7f703662483884a36e30a07773635b45
root <jorgefernandoochoa1@hotmail.com>
2023-04-07 14:48:01 -0700

    1.3

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				return (NULL);
 			new->n = n;
 			if (idx == 0)
-				return (add_dnodeitn(h, n));
+				return (add_dnodeint(h, n));
 			else
 			{
 				new->prev = (*h);
