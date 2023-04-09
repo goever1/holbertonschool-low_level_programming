150a10889c3c56339548b8e9bdcd7e9f473d7ce8
root <jorgefernandoochoa1@hotmail.com>
2023-04-09 13:36:47 -0700

    bit flip

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -9,7 +9,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int nbits;
 
-	for (nbites = 0 || m; n >>= 1, m >>= 1)
+	for (nbits = 0 || m; n >>= 1, m >>= 1;)
 	{
 		if (( n & 1) != (m & 1))
 			nbits++;
