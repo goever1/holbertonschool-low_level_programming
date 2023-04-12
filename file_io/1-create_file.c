b2a505c63aaa5fb00e5a74679a482f834630c21e
root <jorgefernandoochoa1@hotmail.com>
2023-04-12 10:11:14 -0700

    creat file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_RDWR | O_TRUNC);
 	if (fd == -1)
-		fd = opne(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (text_content)
 	{
 		for (t_len = 0; text_content[t_len]; t_len++)
