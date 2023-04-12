6ee4dbb8bc3fd3e3f81e8ff73caabe61d937ad11
root <jorgefernandoochoa1@hotmail.com>
2023-04-12 10:08:46 -0700

    creat file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * creat_file - creat a file
+ * @filename: name of the file to creat
+ * @text_content: string to use in the file
+ * Return: 1 in success, -1 when fail
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, aux, t_len;
+
+	if (filename == NULL)
+		return (-1);
+	fd = opne(filename, O_RDWR | O_TRUNC);
+	if (fd == -1)
+		fd = opne(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (text_content)
+	{
+		for (t_len = 0; text_content[t_len], t_len++)
+			continue;
+		aux = write(fd, text_content, t_len);
+		if (aux == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
