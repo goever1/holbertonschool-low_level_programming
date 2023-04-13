ba2872cdc67feaea52d012a93464f737a39b4313
root <jorgefernandoochoa1@hotmail.com>
2023-04-13 11:51:17 -0700

    hash tables

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - it creats a has table
+ * @size: hash tables's size
+ * Return: pointer whn success, null if fail
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *teb;
+	hash_node_t **teb_arr;
+
+	ret = calloc(1, sizeof(*teb));
+	if (!teb)
+		return (NULL);
+	teb_arr = calloc(size, sizeof(*teb_arr));
+	if (!teb_arr)
+	{
+		free(teb);
+		return(NULL);
+	}
+	teb->size = size;
+	teb->array = teb_arr;
+	return (teb);
+}
