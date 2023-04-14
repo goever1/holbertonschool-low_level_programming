80bf9870b653484e5450e567351054af9c29d27b
root <jorgefernandoochoa1@hotmail.com>
2023-04-14 13:23:03 -0700

    get table

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_table.h"
+/**
+ * hash_table_get - retrives a value associated with a key
+ * @ht: hash table
+ * @key: key 
+ * return: value of key or NULL if fail
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	hash_node_t *actual_node;
+
+	if (ht && key)
+	{
+		idx = key_index((const unsigned char *)key, ht->size);
+		actual_node = (ht->array)[idx];
+		while (actual_node)
+		{
+			if (strcmp(key, actual_node->key) == 0)
+				return (actual_node->value);
+			actual_node = actual_node->next;
+		}
+	}
+	return (NULL);
+}
