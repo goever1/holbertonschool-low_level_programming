7814676035f07512201d4dd15d2a1e9d464186b7
root <jorgefernandoochoa1@hotmail.com>
2023-04-14 13:36:17 -0700

    print table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table to be printed
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx = 0, printed = 0;
+	hash_node_t *actual_node;
+
+	if (ht && ht->array)
+	{
+		printf("{");
+		while (idx < ht->size)
+		{
+			actual_node = (ht->array)[index];
+			while (actual_node)
+			{
+				if (printed > 0)
+					printf(", ");
+				printf("'%s': '%s'", actual_node->key, actual_node->value);
+				printed++;
+				actual_node = actual_node->next;
+			}
+			idx++;
+		}
+		printf("}\n");
+	}
+}
